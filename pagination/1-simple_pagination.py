2ab081f0134336f1b4264f2b20127ba94efff970
Emil-610 <codesolve08112022@gmail.com>
2026-05-23 22:46:51 +0400

    Update 1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
-""" pagination project"""
-
-from typing import Tuple, List
+"""
+Function <index_range> takes two integer arguments
+<page> and <page_size>
+"""
+from typing import Union, Tuple, List
 import csv
 import math
 
 
-index_range = __import__("0-simple_helper_function").index_range
+def index_range(page: int, page_size: int) -> Union[Tuple[int, int], None]:
+    """
+    -Returns a tuple of size 2 containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+    -Page numbers are 1 -indexed, the first page is page 1.
+    """
+    if page and page_size:
+        return (page * page_size - page_size, page * page_size)
 
 
 class Server:
@@ -29,14 +39,18 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ Return the elements with a pagination order """
-        assert type(page) == int and page > 0
-        assert type(page_size) == int and page_size > 0
+        """
+        -Uses assert to verify that both arguments are ints > 0
+        -Uses index_range to find correct indexes to paginate the dataset
+        and return the appropriate page of the dataset (i.e. the list of rows)
+        -Return an empty list if passed arguments are out of range
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start, end = index_range(page, page_size)
-        res_list = []
 
         if start >= len(self.dataset()):
-            return res_list
-        res_list = self.dataset()
-        return res_list[start:end]
+            return []
+
+        return self.dataset()[start:end]
